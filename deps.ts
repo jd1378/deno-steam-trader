4465cebaa58d63517d46f066697ab3f4e4fbda85
Javad Mnjd <javad.mnjd@hotmail.com>
2021-01-28 16:15:23 +0330

    add dependencies

--- deps.ts
+++ deps.ts
@@ -1,1 +1,3 @@
-// export {  } from "https://deno.land/std@0.84.0/";
+export { EventEmitter } from "https://deno.land/std@0.84.0/node/events.ts";
+export { getLanguageInfo } from "https://deno.land/x/language@v0.1.0/mod.ts";
+export { SteamID } from "https://deno.land/x/steamid@v1.0.0/mod.ts";
