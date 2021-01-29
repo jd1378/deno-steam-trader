7f2c10f5c82caebf6a87d98d393af73900847121
Javad Mnjd <javad.mnjd@hotmail.com>
2021-01-29 12:02:06 +0330

    add LFU from velo

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
 export { EventEmitter } from "https://deno.land/std@0.84.0/node/events.ts";
 export { getLanguageInfo } from "https://deno.land/x/language@v0.1.0/mod.ts";
 export { SteamID } from "https://deno.land/x/steamid@v1.0.0/mod.ts";
+export { LFU } from "https://deno.land/x/velo@0.1.5/mod.ts";
