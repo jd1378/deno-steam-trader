ee15efbd0dc386f0c5e9095e40e1603f42c8b1b4
Javad Mnjd <javad.mnjd@hotmail.com>
2021-02-01 10:13:58 +0330

    update deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,9 @@
 export { EventEmitter } from "https://deno.land/std@0.84.0/node/events.ts";
 export { getLanguageInfo } from "https://deno.land/x/language@v0.1.0/mod.ts";
 export { SteamID } from "https://deno.land/x/steamid@v1.0.0/mod.ts";
+export {
+  generateAuthCode,
+  generateConfirmationCode,
+  getDeviceID,
+} from "https://deno.land/x/steamtotp@v2.0.0/mod.ts";
 export { LFU } from "https://deno.land/x/velo@0.1.5/mod.ts";
