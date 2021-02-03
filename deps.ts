bb26704fc0ebe88a3be3f6150fd573b2e48f1664
Javad Mnjd <javad.mnjd@hotmail.com>
2021-02-03 19:11:44 +0330

    update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,5 +1,7 @@
 export { EventEmitter } from "https://deno.land/std@0.85.0/node/events.ts";
 export { randomBytes } from "https://deno.land/std@0.85.0/node/crypto.ts";
+export { Buffer } from "https://deno.land/std@0.85.0/node/buffer.ts";
+
 export { getLanguageInfo } from "https://deno.land/x/language@v0.1.0/mod.ts";
 export { SteamID } from "https://deno.land/x/steamid@v1.0.0/mod.ts";
 export {
@@ -18,5 +20,8 @@ export {
 export { wrapFetch as wrapFetchWithHeaders } from "https://deno.land/x/fetch_goody@v1.1.0/mod.ts";
 
 export { RSA } from "https://deno.land/x/god_crypto@v1.4.9/src/rsa/mod.ts";
+export { AES } from "https://deno.land/x/god_crypto@v1.4.9/src/aes/mod.ts";
 export { RSAKey } from "https://deno.land/x/god_crypto@v1.4.9/src/rsa/rsa_key.ts";
 export { get_key_size as getKeySize } from "https://deno.land/x/god_crypto@v1.4.9/src/helper.ts";
+
+export { getMachineId } from "https://deno.land/x/machine_id@v0.3.0/mod.ts";
