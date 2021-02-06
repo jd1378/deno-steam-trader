58eeedc5bd9035ac6082ebea6f616b8535b7cafa
Javad Mnjd <javad.mnjd@hotmail.com>
2021-02-06 14:08:48 +0330

    add deps

--- deps.ts
+++ deps.ts
@@ -1,6 +1,8 @@
 export { EventEmitter } from "https://deno.land/std@0.85.0/node/events.ts";
 export { randomBytes } from "https://deno.land/std@0.85.0/node/crypto.ts";
 export { Buffer } from "https://deno.land/std@0.85.0/node/buffer.ts";
+export { deferred } from "https://deno.land/std@0.85.0/async/deferred.ts";
+export type { Deferred } from "https://deno.land/std@0.85.0/async/deferred.ts";
 
 export { getLanguageInfo } from "https://deno.land/x/language@v0.1.0/mod.ts";
 export { SteamID } from "https://deno.land/x/steamid@v1.1.1/mod.ts";
