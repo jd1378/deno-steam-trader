3d82cb34b6384a3279b9af209460cc2be86e1461
Javad Mnjd <javad.mnjd@hotmail.com>
2021-02-09 07:06:45 +0330

    update exports

--- mod.ts
+++ mod.ts
@@ -1,3 +1,8 @@
 export * from "./src/trade_manager.ts";
+export * from "./src/enums/EConfirmationMethod.ts";
+export * from "./src/enums/EOfferFilter.ts";
+export * from "./src/enums/EResult.ts";
+export * from "./src/enums/ETradeOfferState.ts";
+export * from "./src/enums/ETradeStatus.ts";
 export { CryptStorage } from "./src/crypt_storage.ts";
 export { Storage } from "./src/storage.ts";
