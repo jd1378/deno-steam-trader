ddc9e3570265167dd14aacbf84303b46fada41f9
Javad Mnjd <javad.mnjd@hotmail.com>
2021-03-22 20:40:50 +0430

    use evt instead of event emitter and update deps

--- deps.ts
+++ deps.ts
@@ -1,7 +1,8 @@
-export { EventEmitter } from "https://deno.land/std@0.86.0/node/events.ts";
-export { randomBytes } from "https://deno.land/std@0.86.0/node/crypto.ts";
-export { Buffer } from "https://deno.land/std@0.86.0/node/buffer.ts";
-export { dirname, resolve } from "https://deno.land/std@0.86.0/node/path.ts";
+export { Evt } from "https://deno.land/x/evt@v1.9.12/mod.ts";
+
+export { randomBytes } from "https://deno.land/std@0.91.0/node/crypto.ts";
+export { Buffer } from "https://deno.land/std@0.91.0/node/buffer.ts";
+export { dirname, resolve } from "https://deno.land/std@0.91.0/node/path.ts";
 
 export { getLanguageInfo } from "https://deno.land/x/language@v0.1.0/mod.ts";
 export { SteamID } from "https://deno.land/x/steamid@v1.1.1/mod.ts";
