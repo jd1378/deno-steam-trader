6d95790564c2244530e117d4acf6edcf7c67917d
Javad Mnjd <javad.mnjd@hotmail.com>
2021-07-10 15:01:34 +0430

    update deps

--- deps.ts
+++ deps.ts
@@ -1,6 +1,7 @@
-export { randomBytes } from "https://deno.land/std@0.91.0/node/crypto.ts";
-export { Buffer } from "https://deno.land/std@0.91.0/node/buffer.ts";
-export { dirname, resolve } from "https://deno.land/std@0.91.0/node/path.ts";
+export { randomBytes } from "https://deno.land/std@0.100.0/node/crypto.ts";
+export { Buffer } from "https://deno.land/std@0.100.0/node/buffer.ts";
+export { dirname, resolve } from "https://deno.land/std@0.100.0/node/path.ts";
+export { delay } from "https://deno.land/std@0.100.0/async/delay.ts";
 
 export { getLanguageInfo } from "https://deno.land/x/language@v0.1.0/mod.ts";
 export { SteamID } from "https://deno.land/x/steamid@v1.1.1/mod.ts";
@@ -19,10 +20,15 @@ export {
 } from "https://deno.land/x/another_cookiejar@v4.0.0/mod.ts";
 export type { CookieOptions } from "https://deno.land/x/another_cookiejar@v4.0.0/mod.ts";
 
-export type { ExtendedRequestInit } from "https://deno.land/x/fetch_goody@v3.0.1/mod.ts";
+export type { ExtendedRequestInit } from "https://deno.land/x/fetch_goody@v5.0.0/mod.ts";
 export {
   wrapFetch as wrapFetchWithHeaders,
-} from "https://deno.land/x/fetch_goody@v3.0.1/mod.ts";
+} from "https://deno.land/x/fetch_goody@v5.0.0/mod.ts";
+
+import {
+  wrapFetch as wrapFetchWithTimeout,
+} from "https://deno.land/x/fetch_goody@v5.0.0/mod.ts";
+export const fetchWithTimeout = wrapFetchWithTimeout();
 
 export { RSA } from "https://deno.land/x/god_crypto@v1.4.9/src/rsa/mod.ts";
 export { AES } from "https://deno.land/x/god_crypto@v1.4.9/src/aes/mod.ts";
@@ -34,9 +40,11 @@ export { getMachineId } from "https://deno.land/x/machine_id@v0.3.0/mod.ts";
 export { DOMParser } from "https://deno.land/x/deno_dom@v0.1.5-alpha/deno-dom-wasm.ts";
 export type { Element } from "https://deno.land/x/deno_dom@v0.1.5-alpha/deno-dom-wasm.ts";
 
-export { Evt, to } from "https://deno.land/x/evt@v1.9.12/mod.ts";
+export { Evt, to } from "https://deno.land/x/evt@v1.9.14/mod.ts";
 
-export type { WebSocketClient } from "https://deno.land/x/websocket@v0.1.0/mod.ts";
-export { StandardWebSocketClient } from "https://deno.land/x/websocket@v0.1.0/mod.ts";
+export type { WebSocketClient } from "https://deno.land/x/websocket@v0.1.2/mod.ts";
+export {
+  StandardWebSocketClient,
+} from "https://deno.land/x/websocket@v0.1.2/mod.ts";
 
 export { Semaphore } from "https://deno.land/x/semaphore@v1.1.0/mod.ts";
